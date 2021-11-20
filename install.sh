c1a6b0f39feec588e8c014a2e9dec6f5c0c780b5
vincemann <v.conrad98@web.de>
2021-11-20 14:18:15 +0100

    update install script to resolve possible ssl errors

--- install.sh
+++ install.sh
@@ -30,7 +30,9 @@ fi
 sudo apt install -y python3-pip
 sudo apt install -y python3-tk
 # sudo python3 -m pip install -r ./requirements.txt
-sudo pip3 install -r ./requirements.txt
+# sudo pip3 install -r ./requirements.txt
+# https://stackoverflow.com/questions/49324802/pip-always-fails-ssl-verification
+sudo python3 -m pip install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org -r requirements.txt
 
 load_libs()
 {
