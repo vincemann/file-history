7a58b32118d6dfb35c5cd0ecda2a66b94d6f557e
vincemann <v.conrad98@web.de>
2021-11-20 14:16:50 +0100

    update python dependencies

---
+++ update-requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# https://stackoverflow.com/questions/39577984/what-is-pkg-resources-0-0-0-in-output-of-pip-freeze-command
+source ./venv/bin/activate
+pip freeze | grep -v "pkg-resources" > requirements.txt
+deactivate
