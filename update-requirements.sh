487ff720853c365108ce6cef2fbf03327ac12f22
vincemann <v.conrad98@web.de>
2021-11-21 13:00:48 +0100

    add update requirements script

--- update-requirements.sh
+++ update-requirements.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # https://stackoverflow.com/questions/39577984/what-is-pkg-resources-0-0-0-in-output-of-pip-freeze-command
 source ./venv/bin/activate
 pip freeze | grep -v "pkg-resources" > requirements.txt
